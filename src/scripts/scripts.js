639d9827310ba8748375e7b070cc741328e4a700
emilio-manoel <emilio.trabalho555@gmail.com>
2026-01-29 12:31:39 -0300

    tela de reiniciar

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -5,34 +5,53 @@ const status = {
         pontos: document.querySelector("#pontos"),
         tempo: document.querySelector("#tempo"),
         vidas: document.querySelector("#vidas"),
-    }, 
+        painel: document.querySelector(".painel"),
+        painelGameOver: document.querySelector(".painel-game-over"),
+        resultadoFinal: document.querySelector("#resultado-final"),
+        botaReiniciar: document.querySelector("#restart-button"),
+    },
     valor: {
         tempo: null,
         contagemRegressivaId: setInterval(contagemRegressiva, 1000),
-        gameVelocidade: 1000,
+        gameVelocidade: 850,
         posicaoAtaque: 0,
         resultado: 0,
         tempoRestante: 60,
+        vidaJogador: 3,
     },
 };
 
-function tocarSom(){
-   let audio = new Audio("./src/assets/audios/hit.m4a");
-   audio.volume = 0.2;
-   audio.play(); 
+function tocarSom() {
+    let audio = new Audio("./src/assets/audios/hit.m4a");
+    audio.volume = 0.2;
+    audio.play();
 }
 
-function contagemRegressiva(){
+function gameOver() {
+    clearInterval(status.valor.contagemRegressivaId);
+    clearInterval(status.valor.tempo);
+    let audio = new Audio("./src/assets/audios/game-over.m4a");
+    audio.volume = 0.2;
+    audio.play();
+
+    status.vizualizacao.painel.style.display = "none";
+    status.vizualizacao.painelGameOver.style.display = "flex";
+    status.vizualizacao.resultadoFinal.textContent = status.valor.resultado;
+}
+
+function restartGame() {
+    window.location.reload();
+}
+
+function contagemRegressiva() {
     status.valor.tempoRestante--;
     status.vizualizacao.tempo.textContent = status.valor.tempoRestante;
-    if(status.valor.tempoRestante <= 0){
-        clearInterval(status.valor.contagemRegressivaId);
-        clearInterval(status.valor.tempo);
-        alert("Game Over! O seu resultado foi de: " + status.valor.resultado + " pontos");
+    if (status.valor.tempoRestante <= 0) {
+        gameOver();
     }
 }
 
-function quadradoAleatorio(){
+function quadradoAleatorio() {
     status.vizualizacao.quadrado.forEach((quadrado) => {
         quadrado.classList.remove("inimigo");
     });
@@ -43,24 +62,32 @@ function quadradoAleatorio(){
     status.valor.posicaoAtaque = quadradoAleatorio.id;
 }
 
-function moverInimigo(){
- status.valor.tempo = setInterval(quadradoAleatorio, status.valor.gameVelocidade);
+function moverInimigo() {
+    status.valor.tempo = setInterval(quadradoAleatorio, status.valor.gameVelocidade);
 }
 
-function esperandoClick (){
+function esperandoClick() {
     status.vizualizacao.quadrado.forEach((quadrado) => {
-     quadrado.addEventListener("click", () => {
-        if(quadrado.id === status.valor.posicaoAtaque){
-            status.valor.resultado++;
-            status.vizualizacao.pontos.textContent = status.valor.resultado;
-            status.valor.posicaoAtaque = null;
-            tocarSom();
-        }
-     });
+        quadrado.addEventListener("click", () => {
+            if (quadrado.id === status.valor.posicaoAtaque) {
+                status.valor.resultado++;
+                status.vizualizacao.pontos.textContent = status.valor.resultado;
+                status.valor.posicaoAtaque = null;
+                tocarSom();
+            } else {
+                status.valor.vidaJogador--;
+                status.vizualizacao.vidas.textContent = `x${status.valor.vidaJogador}`;
+
+                if (status.valor.vidaJogador <= 0) {
+                    gameOver();
+                }
+            }
+        });
     });
 }
 
-function iniciarJogo(){
+function iniciarJogo() {
+    status.vizualizacao.botaReiniciar.addEventListener("click", restartGame);
     moverInimigo();
     esperandoClick();
 }
